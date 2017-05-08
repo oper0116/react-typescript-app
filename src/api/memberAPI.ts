328d30e859c2afbf6d33239fb0e05f475890a432
oper0116 <oper0116@naver.com>
2017-05-08 10:11:48 +0900

    merge conflict 해결을 위한 commit

--- src/api/memberAPI.ts
+++ src/api/memberAPI.ts
@@ -1,36 +1,57 @@
-import * as $ from 'jquery';
-import * as Q from 'q';
+import * as _ from 'lodash';
 import MemberEntity from './memberEntity';
 import MembersMockData from './memberMockData';
 
 class MemberAPI {
 
+  private _idSeed: number;
+
+  public constructor() {
+    this._idSeed = 20;
+  }
+
+  public _generateId(): number {
+    return this._idSeed++;
+  }
+
   public getAllMembers(): Array<MemberEntity> {
     return this._clone(MembersMockData);
   };
 
-  public getAllMembersAsync(): Q.Promise<MemberEntity[]> {
-    var deferred = Q.defer<Array<MemberEntity>>();
-    $.getJSON('https://api.github.com/orgs/lemoncode/members', function(data: any){
-      var members: Array<MemberEntity>;
-      members = data.map((d: any) => {
-        var member : MemberEntity = new MemberEntity();
-        member.id =  d.id;
-        member.login = d.login;
-        member.avatarUrl = d.avatar_url;
-
-        return member;
-      });
-
-      deferred.resolve(members);
-    });
-
-    return deferred.promise;
+  public saveAuthor(member: MemberEntity) {
+    console.log('[Ajax Call]');
+    if (member.id !== -1) {
+      var existingAuthorIndex = _.indexOf(MembersMockData, _.find(MembersMockData, {id: member.id}));
+      MembersMockData.splice(existingAuthorIndex, 1, member);
+    } else {
+      member.id = this._generateId();
+      MembersMockData.push(this._clone(member));
+    }
   }
+
+  // public getAllMembersAsync(): Q.Promise<MemberEntity[]> {
+  //   var deferred = Q.defer<Array<MemberEntity>>();
+  //   $.getJSON('https://api.github.com/orgs/lemoncode/members', function(data: any){
+  //     var members: Array<MemberEntity>;
+  //     members = data.map((d: any) => {
+  //       var member : MemberEntity = new MemberEntity();
+  //       member.id =  d.id;
+  //       member.login = d.login;
+  //       member.avatarUrl = d.avatar_url;
+  //
+  //       return member;
+  //     });
+  //
+  //     deferred.resolve(members);
+  //   });
+  //
+  //   return deferred.promise;
+  // }
 
   private _clone(item: Object) {
     return JSON.parse(JSON.stringify(item));
   };
+
 }
 
 export default new MemberAPI();
--- src/api/memberEntity.ts
+++ src/api/memberEntity.ts
@@ -2,4 +2,10 @@ export default class MemberEntity {
   public id: number;
   public login: string;
   public avatarUrl: string;
+
+  constructor() {
+    this.id = -1;
+    this.login = '';
+    this.avatarUrl = '';
+  }
 }
